9f2402c3246e5170202ec5e6d1419ca29e66d2ce
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-10-18 17:56:11 +0200

    Added a safe area view.n Create a navigation container.  Added a useState component for the storage of data. Made a new screen for the user to navigate to and to view the results, use the  useState component for initializing the name components. Then made new style for the boxs and ther thing.  Created the button for the transfer of all entered variables and added a log. Also a safe area view component.

--- App.tsx
+++ App.tsx
@@ -1,39 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList, ScrollView, TextInput, Button} from 'react-native';
+import { useState } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator(); 
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={MainScreen}/>
+        <Stack.Screen name='ViewDetails' component={ViewDetails}/>
+      </Stack.Navigator>
+    </NavigationContainer>
     
-    <View style={styles.container}>
-      <Text style={styles.Title}>Welcome to my revision </Text>
-      <Text style={styles.lables}>What is your name?</Text>
-      <TextInput placeholder='Please enter your first and last name 'style={styles.box} ></TextInput>
-      <Text style={styles.lables}>What is your favourite band?</Text>
-      <TextInput placeholder='Enter the name of the band.' style={styles.box} ></TextInput>
-      
-
-      <StatusBar style="auto" />
-    </View>
+    
   );
 }
+ function MainScreen({navigation, route}: any){
+  /*This is meant to create the components where thie information will stored for the purposes fo navigation or populating an array */
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [bandName, setBandName] = useState("");
+  const [favSong, setFavSong] = useState("");
+
+  return(
+  <SafeAreaView>
+    <View style={styles.container}  >
+      <Text style={styles.Title}>Welcome to my revision </Text>
+      <Text style={styles.lables}>What is your name?</Text>
+      <TextInput placeholder='Please enter your first name 'style={styles.box} onChangeText={newText => setFirstName(newText)} />
+      <TextInput style={styles.box}  placeholder='Please enter your last name ' onChangeText={newText => setLastName(newText)}/>
+      <Text style={styles.lables}>What is your favourite band?</Text>
+      <TextInput placeholder='Enter the name of the band.' style={styles.box} onChangeText={newText => setBandName(newText)}/>
+      <TextInput style={styles.lables} placeholder='Enter your favourite song from this band ' onChangeText={newText => setFavSong(newText)}/>
+      
+      <StatusBar style="auto" />
+
+      <Button title='Add User'
+      onPress={() => {navigation.navigate('ViewDetails', { NameSend: firstName, SurnameSend: lastName, BandSend: bandName, SongSend: favSong }) ; 
+        console.log(
+          'First Name' + firstName+
+          'Last Name' + lastName+
+          'Band Name' + bandName+
+          'Favourie Song' + favSong
+        ) 
+
+             
+      }}
+      />
+    </View>
+    </SafeAreaView>
+
+  );
+ }
+ function ViewDetails({ route }: any){
+  const { NameSend: NameGet, SurnameSend: SurnameGet, BandSend: BandGet, SongSend: SongGet } = route?.params || {};
+  return(
+    <View>
+      <Text> Name: {NameGet} {SurnameGet}</Text>
+    
+      <Text> Band Name: {BandGet}</Text>
+      <Text> Song Name: {SongGet}</Text>
+    </View>
+  );
+ }
 
 const styles = StyleSheet.create({
-  container: {
+  container: {  
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9cdff0ff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 
   box:{
-    backgroundColor: '#af438fff',
+    backgroundColor: '#be36962d',
     borderColor: '#e0386aff', 
-    padding: 200
+    padding: 20,
+    borderRadius:23
 
   },
   Title:{
     fontSize: 55,
     fontWeight: 500,
+    fontStyle: 'italic'
   },
    
   lables:{
